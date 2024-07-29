c05c7ba3de05082e9a965409c3d460222851c621
Brynn Hawker <brynn@hawker.me>
2024-07-28 23:23:42 -0600

    fix: wrap dropdown in div

--- packages/api-client/src/components/EnvironmentSelector/EnvironmentSelector.vue
+++ packages/api-client/src/components/EnvironmentSelector/EnvironmentSelector.vue
@@ -31,70 +31,72 @@ const envs = computed(() => {
 })
 </script>
 <template>
-  <ScalarDropdown>
-    <ScalarButton
-      class="font-normal h-full justify-start py-1.5 px-1.5 text-c-1 hover:bg-b-3 text-c-2 w-fit"
-      fullWidth
-      variant="ghost">
-      <h2 class="font-medium m-0 text-sm flex gap-1.5 items-center">
-        {{ activeEnvironment?.name ?? 'No Environment' }}
-        <ScalarIcon
-          class="size-2.5"
-          icon="ChevronDown"
-          thickness="3.5" />
-      </h2>
-    </ScalarButton>
-    <!-- Workspace list -->
-    <template #items>
-      <ScalarDropdownItem
-        class="flex gap-1.5 group/item items-center whitespace-nowrap text-ellipsis overflow-hidden"
-        @click.stop="updateSelected('')">
-        <div
-          class="flex items-center justify-center rounded-full p-[3px] w-4 h-4 group-hover/item:shadow-border"
-          :class="
-            activeWorkspace.activeEnvironmentId === ''
-              ? 'bg-blue text-b-1'
-              : 'text-transparent'
-          ">
+  <div>
+    <ScalarDropdown>
+      <ScalarButton
+        class="font-normal h-auto justify-start py-1.5 px-1.5 text-c-1 hover:bg-b-3 text-c-2 w-fit"
+        fullWidth
+        variant="ghost">
+        <h2 class="font-medium m-0 text-sm flex gap-1.5 items-center">
+          {{ activeEnvironment?.name ?? 'No Environment' }}
           <ScalarIcon
             class="size-2.5"
-            icon="Checkmark"
+            icon="ChevronDown"
             thickness="3.5" />
-        </div>
-        No Environment
-      </ScalarDropdownItem>
-      <ScalarDropdownItem
-        v-for="env in envs"
-        :key="env.uid"
-        class="flex gap-1.5 group/item items-center whitespace-nowrap text-ellipsis overflow-hidden"
-        @click.stop="updateSelected(env.uid)">
-        <div
-          class="flex items-center justify-center rounded-full p-[3px] w-4 h-4 group-hover/item:shadow-border"
-          :class="
-            activeWorkspace.activeEnvironmentId === env.uid
-              ? 'bg-blue text-b-1'
-              : 'text-transparent'
-          ">
-          <ScalarIcon
-            class="size-2.5"
-            icon="Checkmark"
-            thickness="3.5" />
-        </div>
-        {{ env.name }}
-      </ScalarDropdownItem>
+        </h2>
+      </ScalarButton>
+      <!-- Workspace list -->
+      <template #items>
+        <ScalarDropdownItem
+          class="flex gap-1.5 group/item items-center whitespace-nowrap text-ellipsis overflow-hidden"
+          @click.stop="updateSelected('')">
+          <div
+            class="flex items-center justify-center rounded-full p-[3px] w-4 h-4 group-hover/item:shadow-border"
+            :class="
+              activeWorkspace.activeEnvironmentId === ''
+                ? 'bg-blue text-b-1'
+                : 'text-transparent'
+            ">
+            <ScalarIcon
+              class="size-2.5"
+              icon="Checkmark"
+              thickness="3.5" />
+          </div>
+          No Environment
+        </ScalarDropdownItem>
+        <ScalarDropdownItem
+          v-for="env in envs"
+          :key="env.uid"
+          class="flex gap-1.5 group/item items-center whitespace-nowrap text-ellipsis overflow-hidden"
+          @click.stop="updateSelected(env.uid)">
+          <div
+            class="flex items-center justify-center rounded-full p-[3px] w-4 h-4 group-hover/item:shadow-border"
+            :class="
+              activeWorkspace.activeEnvironmentId === env.uid
+                ? 'bg-blue text-b-1'
+                : 'text-transparent'
+            ">
+            <ScalarIcon
+              class="size-2.5"
+              icon="Checkmark"
+              thickness="3.5" />
+          </div>
+          {{ env.name }}
+        </ScalarDropdownItem>
 
-      <!-- Add new environment -->
-      <ScalarDropdownItem
-        v-if="!isReadOnly"
-        class="flex items-center gap-1.5"
-        @click="createNewEnvironment">
-        <div class="flex items-center justify-center h-4 w-4">
-          <ScalarIcon
-            class="h-2.5"
-            icon="Add" />
-        </div>
-        <span>Create new environment</span>
-      </ScalarDropdownItem>
-    </template>
-  </ScalarDropdown>
+        <!-- Add new environment -->
+        <ScalarDropdownItem
+          v-if="!isReadOnly"
+          class="flex items-center gap-1.5"
+          @click="createNewEnvironment">
+          <div class="flex items-center justify-center h-4 w-4">
+            <ScalarIcon
+              class="h-2.5"
+              icon="Add" />
+          </div>
+          <span>Create new environment</span>
+        </ScalarDropdownItem>
+      </template>
+    </ScalarDropdown>
+  </div>
 </template>
